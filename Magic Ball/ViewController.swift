bab37080d9f52c306bb43bc72e78c4c0b95d0ce3
Rylan Ugalde-Lopez <rylanu1@109d25pf0fzf8j7.student.cssd.ab.ca>
2019-02-07 14:32:11 -0700

    Initial Commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Rylan Ugalde-Lopez on 2019-02-07.
+//  Copyright © 2019 Rylan Ugalde-Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
