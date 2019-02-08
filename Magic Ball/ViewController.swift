076dcc6f3ca85b7c90f6f1dfca869fc4fa783a3b
Your Name <you@example.com>
2019-02-08 13:38:44 -0700

    jh

--- Magic Ball/ViewController.swift
+++ Magic Ball/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+//import UIKit
+
+class ViewController: UIViewController {
+    //Segment for ball faces
+    
+    var randomMagicIndex : Int = 0
+    
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    //
+    //ball Image
+    
+    @IBOutlet weak var ballImageView: UIImageView!
+    //
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBallimages()
     }
+    
+    
+    //Button Code Segment
 
-
+    @IBAction func shakeBall(_ sender: UIButton) {
+         updateBallimages()
+    }
+    
+    // This segment changes Dice faces
+    func updateBallimages () {
+        randomMagicIndex = Int(arc4random_uniform(5))
+        
+        
+        ballImageView.image = UIImage(named: ballArray[randomMagicIndex])
+        
+        
+    }
+    
+    
+    // Function for when shake is detected.
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateBallimages()
+    }
 }
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
 
